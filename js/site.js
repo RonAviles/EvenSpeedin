28268082f875dade566604d1605eefacfa5c4344
RonAviles <89873733+RonAviles@users.noreply.github.com>
2021-09-11 19:42:46 -0500

    Completed V1
    Even Speedin v1.0 complete.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,47 @@
-//Call hello world
-function HelloWorld(){
-    alert("hello world");
+//get the values from the interface
+//starts controller function
+function getValues() {
+//get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+        //parse into integers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter intergers");
+    }
+    //we call generateNumbers
+    //we call displayNumbers
+}
+
+//generate numbers from the startValue to the endValue
+//logic functions
+function generateNumbers(sValue, eValue) {
+    let numbers = [];
+    //we want to get all numbers from start to end
+    //for loop
+    for (let i = sValue; i <= eValue; i++){
+        //this will execute in a loop until i = eValue
+        numbers.push(i);
+    }
+    return numbers;
+}
+//display numbers and mark even numbers bold 
+//display or view function 
+function displayNumbers(numbers) {
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        let className = "even";
+        if (number % 2 == 0 && number != 0) {
+            className = "even";
+        } else {
+        className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
